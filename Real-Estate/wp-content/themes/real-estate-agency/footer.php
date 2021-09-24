d5a134d17071db16cdf15cce56c1ff7c45b1844a
KhanhGamer1601 <KhanhGamer1601@gmail.com>
2021-09-24 23:11:29 +0700

    'App'

--- Real-Estate/wp-content/themes/real-estate-agency/footer.php
+++ Real-Estate/wp-content/themes/real-estate-agency/footer.php
@@ -43,29 +43,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 						<?php dynamic_sidebar( 'footer-4' ); ?>
 					</div>
 				<?php endif; ?>
-
 			</div>		
-		<div class="site-info">
-		<?php
-		if ( get_theme_mod( 'real_estate_agency_copyright' ) ) {
-			echo esc_html( get_theme_mod( 'code_copyright_text' ) );
-		}
-		else { ?>
-			<a class="powered" href="<?php echo esc_url( __( 'https://wordpress.org/', 'real-estate-agency' ) ); ?>">
-				<?php
-				esc_html_e( 'Powered by WordPress', 'real-estate-agency' );
-				?>
-			</a>
-			<p>
-				<?php esc_html_e( 'All rights reserved', 'real-estate-agency' ); ?>  &copy; <?php bloginfo( 'name' ); ?>			
-				<a title="Seos Theme - Real Estate Agency" href="<?php echo esc_url( 'https://seosthemes.com/', 'real-estate-agency' ); ?>" target="_blank"><?php esc_html_e( ' Real Estate Agency Theme by Seos Themes', 'real-estate-agency' ); ?></a>
-			</p>
-		<?php } ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-	<?php if( get_theme_mod( 'activate_back_to_top', true ) ) { real_estate_agency_to_top(); } ?>
-
 <?php wp_footer(); ?>
-
 </body>
 </html>
